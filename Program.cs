f3a6a5afc3876104b4ec02346f47083846160460
Thaju Mohammed <thaju.mohammed@aspiresys.kl>
2024-12-10 09:34:07 +0530

    Initial commit of my C# program

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+class Program
+{
+    static void Main()
+    {
+        ConcurrentStack<string> stack = new ConcurrentStack<string>();
+
+        stack.Push("First");
+        stack.Push("Second");
+        stack.Push("Third");
+
+        if (stack.TryPop(out string result))
+        {
+            Console.WriteLine($"Popped: {result}");
+        }
+
+        if (stack.TryPeek(out string top))
+        {
+            Console.WriteLine($"Top element: {top}");
+        }
+
+        stack.PushRange(new string[] { "Fourth", "Fifth", "Sixth" });
+
+        string[] range = new string[2];
+        if (stack.TryPopRange(range, 0, 2) > 0)
+        {
+            Console.WriteLine("Popped range: " + string.Join(", ", range));
+        }
+
+        Console.WriteLine($"Count: {stack.Count}");
+    }
+}
